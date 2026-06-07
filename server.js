a07550c10408e7a0a67b20c7b476623b07084da7
Tomate1519 <chrisca88@gmail.com>
2026-06-07 07:26:32 -0500

    Servidor basico Node

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || "text/plain";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Archivo no encontrado");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
